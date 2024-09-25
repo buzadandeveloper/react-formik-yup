5ee687d9844baa2d45cbb6ebcc0ef212dff397f2
Buza Dan <buzdandeveloper@gmail.com>
2024-09-25 14:23:46 +0300

    feat: configure App for global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./GlobalStyles/GlobalStyles";
+import { Auth } from "./components/Auth/Auth";
 function App() {
-
+  const theme = {
+    colors: {
+      primary: "#66BB69",
+      black: "#263238",
+      d_grey: "#4D4D4D",
+      grey: "#717171",
+      l_grey: "#89939E",
+      white: "#FFFFFF",
+      shade: "#28CB8B",
+      silver: "#F5F7FA",
+    },
+  };
   return (
-    <>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+        <Auth />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
